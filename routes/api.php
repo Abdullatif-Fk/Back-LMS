0ceadd94e00107998227cbf73b05d7a2e4ec3a48
Hussein Moussa <hseinmoussa98@gmail.com>
2020-12-29 09:21:22 +0200

    ok

--- routes/api.php
+++ routes/api.php
@@ -11,17 +11,6 @@ use App\Http\Controllers\Edit_Student;
 
 
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
