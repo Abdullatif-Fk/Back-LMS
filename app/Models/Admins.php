a1c9484102e13e0881c6ad4d5f1c45d3377fffae
Hussein Moussa <hseinmoussa98@gmail.com>
2021-01-03 04:10:01 +0200

    prepare seeder for production

--- app/Models/Admins.php
+++ app/Models/Admins.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Admins extends Model
 { public $timestamps = false;
+    protected $table='Admins';
     use HasFactory;
 
     protected $fillable = [
